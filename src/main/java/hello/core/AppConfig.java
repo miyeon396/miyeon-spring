4b7a64067d01f6819990b88c64ce11e415556474
miyeon.yeon <miyeon396@gmail.com>
2022-03-31 21:31:11 +0900

    spring : appConfig refactoring
    - AppConfig를 보면 역할과 구현 클래스가 한눈에 들어오도록 변경
    - 메서드명을 보면 역할이 보임. 안에 return을 보면 구현이 보임.
    - 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있어짐.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,14 +11,24 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     //App전체를 설정하고 구성한다는 뜻.
+    // 리팩토링 후 : 메서드명을 보면 역할이 보임. 안에 return을 보면 구현이 보임.
 
     public MemberService memberService() { //멤버 서비스를 여기서 만듦.
         //생성자 주입 방식으로 변경.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemoryMemberRepository getMemberRepository() {
+        //나는 멤버 리포지토리 쓰는데 memoryMemberRepo쓸거야
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() { //구체적인 것은 여기서 결정.
-        return  new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return  new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
