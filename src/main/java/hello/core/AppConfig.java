fcc95b33fe3ea5e235a9d9d36eef0437a5fcdcc0
miyeon.yeon <miyeon396@gmail.com>
2022-03-31 23:24:56 +0900

    spring : spring으로 전환 간단 예제

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,26 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //어플리케이션의 구성 정보.
 public class AppConfig {
 
     //App전체를 설정하고 구성한다는 뜻.
     // 리팩토링 후 : 메서드명을 보면 역할이 보임. 안에 return을 보면 구현이 보임.
 
+    @Bean //빈들은 스프링 컨테이너에 등록. 기본적으로 이름은 메서드 이름이 등록이 된다.
     public MemberService memberService() { //멤버 서비스를 여기서 만듦.
         //생성자 주입 방식으로 변경.
         return new MemberServiceImpl(getMemberRepository());
     }
 
-    private MemoryMemberRepository getMemberRepository() {
+    @Bean
+    public MemoryMemberRepository getMemberRepository() {
         //나는 멤버 리포지토리 쓰는데 memoryMemberRepo쓸거야
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() { //구체적인 것은 여기서 결정.
         return  new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy(); //정률할인으로 바꾸길 원하는 경우 여기만 고쳐주면 됨.
