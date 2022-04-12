786d36e3c31d54947a90a77d1bc299cbfe907cc9
miyeon.yeon <miyeon396@gmail.com>
2022-04-12 16:35:44 +0900

    spring : 롬복 쓰면서 Qualifier 쓸 수 있도록 추가

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
+//@MainDiscountPolicy
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
