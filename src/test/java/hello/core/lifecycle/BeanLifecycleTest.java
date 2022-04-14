a5185292627a91458ae4a17699afcc1e8bb276df
miyeon.yeon <miyeon396@gmail.com>
2022-04-14 23:04:43 +0900

    spring : 빈 생명주기 콜백 예제

---
+++ src/test/java/hello/core/lifecycle/BeanLifecycleTest.java
@@ -0,0 +1,26 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class); //ConfigurableApplicationContext로 쓰거나
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient(); //스프링 빈 등록할 때 생성자 호출 하겠죠
+            networkClient.setUrl("http://hello-spring.dev"); //외부에서 값을 세팅하고 나서 초기화를 해줘야함... 수정자 주입을 통해서 주입
+            return networkClient;
+        }
+    }
+}
