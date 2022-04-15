fa2c7b6c8519bedcbe304ea609c57236295915cb
miyeon.yeon <miyeon396@gmail.com>
2022-04-15 15:20:23 +0900

    spring : 웹 스코프 request 주입 에러 해결 with provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; //requestScope -> 의존관계 주입 받고 있는 중
+    private final ObjectProvider<MyLogger> myLoggerProvider; //이렇게 하면 myLogger을 주입 받는게 아니라 myLogger을 찾을 수 있는애들 DependencyLookup하게됨
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
